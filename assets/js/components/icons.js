19692dc124667e910c05e90fd746ba720cfde8ef
Aki <aki@finquteam.com>
2021-05-17 09:47:57 +0300

    ifx

---
+++ assets/js/components/icons.js
@@ -0,0 +1,19 @@
+import MoveLeft1 from '@streamlinehq/streamlinehq/img/streamline-light/move-left-1-fSZYq5.svg';
+import MoveRight1 from '@streamlinehq/streamlinehq/img/streamline-light/move-right-1-fSZYYL.svg';
+import MoveDown1 from '@streamlinehq/streamlinehq/img/streamline-light/move-down-1-fSZQ4o.svg';
+import MoveUp1 from '@streamlinehq/streamlinehq/img/streamline-light/move-up-1-hNEO9b.svg';
+import ShrinkHorizontal from '@streamlinehq/streamlinehq/img/streamline-light/shrink-horizontal-y7qSce.svg';
+import ShrinkVertical from '@streamlinehq/streamlinehq/img/streamline-light/shrink-vertical-y7qZWz.svg';
+import ArrowLeft1 from '@streamlinehq/streamlinehq/img/streamline-light/arrow-left-1-itskPx.svg';
+import ArrowRight1 from '@streamlinehq/streamlinehq/img/streamline-light/arrow-right-1-Ei8Dl0.svg'
+
+export default {
+	alignLeft: MoveLeft1,
+	alignCenter: ShrinkHorizontal,
+	alignRight: MoveRight1,
+	alignTop: MoveUp1,
+	alignMiddle: ShrinkVertical,
+	alignBottom: MoveDown1,
+	angleLeft: ArrowLeft1,
+	angleRight: ArrowRight1
+}
